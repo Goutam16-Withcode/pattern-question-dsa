5256d8045079423f52bd5b7497588e859878be7f
Goutam Sharma <goutamshaarma001212@gmail.com>
2026-08-09 17:30:35 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int l = 0 , r = people.size()-1;
+        int count = 0;
+        while(l<=r){
+            int sum = people[l] + people[r];
+            if(sum<=limit){
+                l++;
+            }
+            r--;
+            count++;
+        }
+        return count;
+    }
+};
