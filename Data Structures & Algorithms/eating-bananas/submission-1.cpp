d5302776a492fb470f03b06133506fbe9bb9d738
Goutam Sharma <goutamshaarma001212@gmail.com>
2026-08-21 21:20:16 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(vector<int>& piles , int h , int speed){
+        long long hours = 0;
+        for(int banana : piles){
+            hours+=(banana+speed-1)/speed;
+            if(hours > h) return false;
+        }
+        return hours<=h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = *max_element(piles.begin(),piles.end());
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(isValid(piles,h,mid)){
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return low;
+    }
+};
